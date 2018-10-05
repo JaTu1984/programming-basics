778c362a3a3f42a2654b8586e6f499dfa2671c18
Janne M Tuomela <JaTu1984@gmail.com>
2018-10-05 18:16:36 +0300

    Added LoopTask3.4

--- looptasks/LoopTask3-2/LoopTask3-2/Program.cs
+++ looptasks/LoopTask3-2/LoopTask3-2/Program.cs
@@ -6,7 +6,36 @@ namespace LoopTask3_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen");
+
+            int number = 0;
+            int i = 1;
+            int sum = 0;
+
+         //check that number is positive number
+            do
+            {
+                Console.Write("Anna kokonaisluku: ");
+                string userInput = Console.ReadLine();
+                bool isNumber = int.TryParse(userInput,out number);
+                
+                
+                if (number < 1 || !isNumber)
+                {
+                    Console.WriteLine("Virheellinen syöte");
+                }
+                
+
+            } while (number < 1);
+            
+            while (i <= number)
+            {
+                sum = sum + i; //fact=*i
+                i = i + 1;      //i++;
+
+            }
+            
+            Console.WriteLine($"Kokonaisluvun {number} summa = {sum}");
         }
     }
 }
---
+++ looptasks/LoopTask3-4/LoopTask3-4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace LoopTask3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen, myös negatiivisilla luvuilla");
+
+            int number = 0;
+
+            int i = 0;
+
+            int sum = 0;
+
+
+
+            do
+
+            {
+
+                Console.Write("Syötä luku: ");
+
+                bool isNumber = int.TryParse(Console.ReadLine(), out number);
+
+                if (!isNumber)
+
+                {
+
+                    Console.Write("Virheellinen syöte!");
+
+                }
+
+                else
+
+                {
+
+                    break;
+
+                }
+
+            } while (true);
+
+
+
+            while (i != number)
+
+            {
+
+                if (number > 0)
+
+                {
+
+                    i++;
+
+                }
+
+                else
+
+                {
+
+                    i--;
+
+                }
+
+                sum = sum + i;
+
+            }
+
+            Console.WriteLine($"Vastaus: {sum}");
+
+
+
+
+
+
+        }
+    }
+}
