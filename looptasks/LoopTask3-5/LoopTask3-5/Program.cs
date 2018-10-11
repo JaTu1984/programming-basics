3656c17c7efb05588092043649c4a2b8c8bb0152
Janne M Tuomela <JaTu1984@gmail.com>
2018-10-11 12:51:43 +0300

    LoopTask 4.2 created and LoopTask 4.1 increased.

--- looptasks/LoopTask3-5/LoopTask3-5/Program.cs
+++ looptasks/LoopTask3-5/LoopTask3-5/Program.cs
@@ -10,6 +10,8 @@ namespace LoopTask3_5
             int i = 0;
             int evenSum = 0;
             int oddSum = 0;
+            string evenStr = string.Empty; //2+4+6+8+10 =
+            string oddStr = string.Empty; //1+3+5+7+9 =
             Console.WriteLine("Ohjelma laskee parittomien ja parillisten lukujen summan, myös negatiivisesti");
             Console.Write("Syötä luku: ");
             number = int.Parse(Console.ReadLine());
@@ -28,14 +30,23 @@ namespace LoopTask3_5
                 if (i % 2 == 0)
                 {
                     evenSum += i;
+                    if (i != number)
+                    evenStr = $"{evenStr}{i} + "; 
+                    else
+                    evenStr = $"{evenStr}{i}";
                 }
                 else
                 {
                     oddSum += i;
+                    if (i != number - 1)
+                    oddStr = $"{oddStr}{i} + ";
+                    else
+                    oddStr = $"{oddStr}{i}";
                 }
             }
-            Console.WriteLine($"Parillisten summa on {evenSum}");
-            Console.WriteLine($"Parittomien summa on {oddSum}");
+            Console.WriteLine($"{oddStr} = {oddSum}");
+            Console.WriteLine($"{evenStr} = {evenSum}");
+            
         }
     }
 }
