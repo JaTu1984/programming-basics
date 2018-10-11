07999d8ac6915939f90db3dda1176654f2fd5dfc
Janne M Tuomela <JaTu1984@gmail.com>
2018-10-11 13:24:49 +0300

    LoopTask4.2 completed.

--- looptasks/LoopTask4-2/LoopTask4-2/Program.cs
+++ looptasks/LoopTask4-2/LoopTask4-2/Program.cs
@@ -6,7 +6,20 @@ namespace LoopTask4_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1-9");
+            Console.WriteLine("Aloita ohjelman suoritus painamalla jotain näppäintä");
+            Console.ReadKey();
+
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write($"{i}\t");
+                for (int j = 1; j <= 9; j++)
+                {
+                Console.Write($"{i * j}\t");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
