e0d7f1494b2c16a0d824f3e5ead3a8a0be94dd96
Janne M Tuomela <JaTu1984@gmail.com>
2018-10-12 11:58:06 +0300

    LoopTask 4.3  created.

---
+++ looptasks/LoopTask4-3/LoopTask4-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LoopTask4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma, joka tulostaa 20 satunnaista kokonaislukua väliltä [0-50].");
+
+            Random rnd = new Random();
+            int rndNumber;
+            int i = 0;
+            int j = 0;
+       
+
+            do
+            {
+                rndNumber = rnd.Next(0, 51);
+                Console.WriteLine($"{i + 1}.\t{rndNumber}\t");
+                i = i + 1; //i++
+            } while (i < 50);
+        }
+    }
+}
