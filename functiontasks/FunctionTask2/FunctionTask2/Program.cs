85e61e3eeb8e608fe85b9433faa3021e2bcddea7
Janne M Tuomela <JaTu1984@gmail.com>
2018-10-17 16:45:08 +0300

    Synchronizing.

--- functiontasks/FunctionTask2/FunctionTask2/Program.cs
+++ functiontasks/FunctionTask2/FunctionTask2/Program.cs
@@ -7,6 +7,28 @@ namespace FunctionTask2
         static void Main(string[] args)
         {
             Console.WriteLine("Funktio palautta arvonaan kahdesta annetusta luvusta pienemmän");
+
+            int number1;
+            int number2;
+            int i = 0;
+
+            Console.WriteLine("Anna kokonaisluku: ");
+            
+
+            number1 = int.Parse(Console.ReadLine());
+            number2 = int.Parse(Console.ReadLine());
+
+            if (number1 > number2)
+
+                Console.WriteLine($"{number2} on pienempi kuin {number1}");
+
+            if (number2 > number1)
+
+                Console.WriteLine($"{number1} on pienempi kuin {number2}");
+
+            else
+
+                Console.WriteLine("Luvut ovat yhtä suuret");
         }
     }
 }
