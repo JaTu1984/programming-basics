71bc45f0ed544c6c96078db875187c392efd085a
Janne M Tuomela <JaTu1984@gmail.com>
2018-11-03 17:36:22 +0200

    Array Tasks created and done.

---
+++ arraytasks/ArrayTask1/ArrayTask1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace ArrayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = new CultureInfo("sv-SV");
+            CultureInfo.CurrentCulture = culture;
+            Console.WriteLine(DateTime.Now);
+
+            culture = new CultureInfo("fi-FI");
+            CultureInfo.CurrentCulture = culture;
+            Console.WriteLine(DateTime.Now);
+
+            culture = new CultureInfo("us-US");
+            CultureInfo.CurrentCulture = culture;
+            Console.WriteLine(DateTime.Now);
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hinnasto");
+            
+            
+
+            decimal[] productPrice = new decimal[] { 7.96m, 72.0m, 99.90m };
+            int[] productAmount = new int[] {1,3,2};
+            decimal[] productTotalPrice = new decimal[3];
+
+            for (int i = 0; i < productTotalPrice.Length; i++)
+            {
+                productTotalPrice[i] = productAmount[i] * productPrice[i];
+                Console.WriteLine($"Tuote {i + 1}. {productTotalPrice[i]:c}");
+            }
+        }
+    }
+}
---
+++ arraytasks/ArrayTask2/ArrayTask2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma, joka arpoo 100 lukua [0...50] väliltä ja tallettaa ne taulukkoon iT. Arvotut luvut sekä luvuista saatu keskiarvo ilmoitetaan käyttäjälle.");
+            int[] iT = new int[100];
+            Random rnd = new Random();
+            int average = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                iT[i] = rnd.Next(50);
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"{i+1}. {iT[i]}");
+                average = average + iT[i];
+            }
+            Console.WriteLine($"Keskiarvo on {average/100}");
+        }
+    }
+}
