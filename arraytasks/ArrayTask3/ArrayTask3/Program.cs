eb4c8a9c83a40a1248461403f2ba31354ff9c2fa
Janne M Tuomela <JaTu1984@gmail.com>
2018-11-04 13:28:47 +0200

    ArrayTask3 created.

---
+++ arraytasks/ArrayTask3/ArrayTask3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ArrayTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo 7 eri lukua 1-40 ja tulostaa arvotut luvut pienimmästä suurimpaan.");
+
+        }
+    }
+}
