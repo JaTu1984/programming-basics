b078d48fe193de44660744aab4c7c551d24f33f3
Janne M Tuomela <JaTu1984@gmail.com>
2018-11-18 12:21:59 +0200

    Taulukkoharjoitus3.2 done and 3.3 created.

--- arraytasks/Taulukkoharjoitus3.2/Taulukkoharjoitus3.2/Program.cs
+++ arraytasks/Taulukkoharjoitus3.2/Taulukkoharjoitus3.2/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace Taulukkoharjoitus3._2
+namespace Taulukkoharjoitus3._1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma arpoo kaksiulotteiseen taulukkoon luvut 0-20");
+
+            int[,] array = new int[10, 20];
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 20; j++)
+            {
+                array[i, j] = rnd.Next(100);
+            }
+            for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 20; j++)
+            {
+                Console.WriteLine($"[{i + 1}, {j+1}]. {array[i, j]}");
+            }
         }
     }
 }
+
