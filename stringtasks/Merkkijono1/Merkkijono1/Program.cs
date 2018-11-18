688ff2fdb74998aa3748fd0e751e559b7f72c122
Janne M Tuomela <JaTu1984@gmail.com>
2018-11-18 13:00:53 +0200

    Stringtasks created and added Merkkijono1 task.

---
+++ stringtasks/Merkkijono1/Merkkijono1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Merkkijono1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma, joka ilmoittaa käyttäjän syöttämästä syötteestä merkkien lukumäärän.");
+            string userInput = Console.WriteLine("Syötä lause: ");
+            
+
+            
+
+        }
+    }
+}
