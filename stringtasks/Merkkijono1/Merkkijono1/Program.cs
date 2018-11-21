746b46b49481c3de4d376a07c2b2e21a3475b26a
Janne M Tuomela <JaTu1984@gmail.com>
2018-11-21 08:56:10 +0200

    StringTask 1 done and working. String Tasks 2 and 3 in file StringExample.

--- stringtasks/Merkkijono1/Merkkijono1/Program.cs
+++ stringtasks/Merkkijono1/Merkkijono1/Program.cs
@@ -1,17 +1,69 @@
 ﻿using System;
 
-namespace Merkkijono1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Ohjelma, joka ilmoittaa käyttäjän syöttämästä syötteestä merkkien lukumäärän.");
-            
-            
 
-            
+
+namespace StringExample
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+            int len = 0;
+
+            Console.WriteLine("Ánna lause tai merkkijono: ");
+            string userInput = Console.ReadLine();
+
+            len = userInput.Length;
+
+            Console.WriteLine($"Merkkijonossa {userInput} on merkkejä {len} kappaletta");
 
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static int CountLetter(string str)
+
+        {
+
+            int letterCounter = 0;
+
+            for (int i = 0; i < str.Length; i++)
+
+            {
+
+                if (str[i] == 'L')
+
+                    letterCounter++;
+
+            }
+
+            return letterCounter;
+
+        }
+
     }
+
 }
