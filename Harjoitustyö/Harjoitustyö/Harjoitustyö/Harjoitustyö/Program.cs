22a3dc9a76ecbd32345d24a165547f4ecc2a79a7
Janne M Tuomela <JaTu1984@gmail.com>
2018-11-27 08:05:19 +0200

    Added delay and edited some code.

--- Harjoitustyö/Harjoitustyö/Harjoitustyö/Harjoitustyö/Program.cs
+++ Harjoitustyö/Harjoitustyö/Harjoitustyö/Harjoitustyö/Program.cs
@@ -4,42 +4,78 @@ namespace Harjoitustyö
 {
     class Program
     {
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Kansallinen viitenumero");
-
-            Console.WriteLine("Valitse toiminto [1-3]: ");
 
             int userAddedReferenceNumber = 0;
+            
+            do
+            {
 
-            Console.WriteLine($"1. Haluan tarkastaa kansallisen viitenumeron");
-            Console.WriteLine($"2. Haluan luoda kansallisen viitenumeron");
-            Console.WriteLine($"3. Haluan määrittää monta viitenumeroa luodaan ja tulostetaan tiedostoon");
+                Console.WriteLine();
+                Console.WriteLine("Kansallinen viitenumero");
+
+                Console.WriteLine("Valitse toiminto [1-3]: ");
+
+                Console.WriteLine($"1. Haluan tarkastaa kansallisen viitenumeron");
+                Console.WriteLine($"2. Haluan luoda kansallisen viitenumeron");
+                Console.WriteLine($"3. Haluan määrittää monta viitenumeroa luodaan ja tulostetaan tiedostoon");
+
+                //bool isNumber = int.TryParse(Console.ReadKey().KeyChar.ToString(), out int caseSwitch);
+                //if (isNumber)
+
+                int caseSwitch = int.Parse(Console.ReadLine());
+                {
+                    switch (caseSwitch)
+                    {
+                        case 1:
+                            Console.WriteLine("Syötä viitenumero");
+                            userAddedReferenceNumber = int.Parse(Console.ReadLine());
+                            Console.WriteLine($"{userAddedReferenceNumber}");
+                            Console.WriteLine("Viitenumero on oikein");
+                            break;
+                        case 2:
+                            Console.WriteLine("Viitenumero luotu");
+                            break;
+                        case 3:
+                            int viitteet = 0;
+                            int i = 0;
+                            Console.WriteLine("Syötä tiedostoon tulostettavien viitteiden lukumäärä");
+                            viitteet = int.Parse(Console.ReadLine());
+                            for (i = 0; i < viitteet; i++)
+                                Console.WriteLine("viitteet tulostetaan tiedostoon");
+                            break;
+                        default:
+                            Console.WriteLine("Virhe!!!");
+                            break;
+                    }
+                }
+                Console.Clear();
+
+            } while (true);
+
+            
+        }
+
+
+        static void Delay()
+
+        {
+
+            for (int i = 0; i < 3; i++)
 
             {
-                int caseSwitch = int.Parse(Console.ReadLine());
 
-                switch (caseSwitch)
-                {
-                    case 1:
-                        Console.WriteLine("Syötä viitenumero");
-                        userAddedReferenceNumber = int.Parse(Console.ReadLine());
-                        Console.WriteLine($"{userAddedReferenceNumber}");
-                        Console.WriteLine("Viitenumero on oikein");
-                        break;
-                    case 2:
-                        Console.WriteLine("Viitenumero luotu");
-                        break;
-                    default:
-                        int viitteet = 0;
-                        int i = 0;
-                        Console.WriteLine("Syötä tiedostoon tulostettavien viitteiden lukumäärä");
-                        viitteet = int.Parse(Console.ReadLine());
-                        for (i = 0; i < viitteet; i++)
-                        Console.WriteLine("viitteet tulostetaan tiedostoon");
-                        break;
-                }
+                Console.Write(".");
+
+                System.Threading.Thread.Sleep(1000);
+
             }
+
+            Console.WriteLine();
+
         }
     }
 }
